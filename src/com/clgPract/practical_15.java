9221455f1a74a157726588a08140edbd359c290e
Poorvang1353 <poorvangpatel13@gmail.com>
2022-04-13 20:16:05 +0530

    -

---
+++ src/com/clgPract/practical_15.java
@@ -0,0 +1,31 @@
+package com.clgPract;
+
+import java.util.Scanner;
+
+class volume{
+    double a,b,c;
+    public volume(double a){
+        System.out.println("Volume of cube: " + (a*a*a));
+    }
+
+    public volume(double a, double b, double c){
+        System.out.println("Volume of Rectangular Box: " + (a*b*c));
+    }
+}
+
+public class practical_15 {
+    public static void main(String[] args) {
+        double a,b,c;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the side of cube: ");
+        a = s.nextDouble();
+        volume obj = new volume(a);
+        System.out.println("Enter the length, width and height of the rectangular box: ");
+        a = s.nextDouble();
+        b = s.nextDouble();
+        c = s.nextDouble();
+        volume obj_2 = new volume(a,b,c);
+
+
+    }
+}
